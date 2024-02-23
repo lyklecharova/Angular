5ae2839e8bfede07c754843ee6016b8bc0f9e296
lyklecharova <lklecharova9@gmail.com>
2024-02-23 21:06:34 +0200

    UPDATE

--- Components/first-demo-components-app/src/app/app.component.ts
+++ Components/first-demo-components-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-demo-app';
+  colorValueForPlayground = 'blue';
+
+  onOutputFromChild (inputValue:string){
+    console.log('from parent:', inputValue);
+  }
 }
--- Components/first-demo-components-app/src/app/playground/playground.component.ts
+++ Components/first-demo-components-app/src/app/playground/playground.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
+const IMG_URL =  'https://mytanklife.com/wp-content/uploads/2022/09/Siamese-Fighter-Female-removebg-preview-600x600.png';
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css'],
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent implements OnInit, OnDestroy {
+  @Input('color') colorValue = 'white';
+  @Output() onTestOutput = new EventEmitter<string>();
+
   isToggle = false;
   greenBackround = 'background-green';
-  imgUrl =
-    'https://mytanklife.com/wp-content/uploads/2022/09/Siamese-Fighter-Female-removebg-preview-600x600.png';
+  imgUrl = IMG_URL;
+
+  ngOnInit(){
+    console.log('Component Created!');
+  }
+
+  ngAfterViewInit(){
+    console.log('After Init');
+  }
+
+  ngOnDestroy(){
+    // clean up of stuff!!!
+    // Delete
+    console.log('On Destroy');
+  }
+
+  ngDoCheck () {
+    if(this.isToggle === true){
+      console.log({isToggle: this.isToggle});
+    }
+   
+  }
+
   handleClick() {
     console.log('clicked');
     this.isToggle = !this.isToggle;
@@ -17,6 +42,9 @@ export class PlaygroundComponent {
 
   handleInput(usernameValue:string){
     console.log('username: ', usernameValue);
+    console.log(this.colorValue);
+
+    this.onTestOutput.emit(usernameValue);
     
   }
 }
