4a92c0f2fe1420c90937f7b4a8f2aad471885f30
lyklecharova <lklecharova9@gmail.com>
2024-02-23 18:16:24 +0200

    Create component with command

--- Components/first-demo-components-app/src/app/app.module.ts
+++ Components/first-demo-components-app/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NavigationComponent } from './navigation/navigation.components';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   // изреждат се всички компоненти
   declarations: [
     AppComponent,
     NavComponent,
-    NavigationComponent
+    NavigationComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
