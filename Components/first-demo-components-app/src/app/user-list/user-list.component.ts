2c809e7c4d9cd1f8605e520226de97fcf628d4b4
lyklecharova <lklecharova9@gmail.com>
2024-02-23 18:35:51 +0200

    Update

--- Components/first-demo-components-app/src/app/user-list/user-list.component.ts
+++ Components/first-demo-components-app/src/app/user-list/user-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+type User = {
+  name: String,
+  age: number,
+};
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
 
+export class UserListComponent {
+  users = [
+    {name: 'Pesho',age: 18},
+    {name: 'Mitko',age: 22},
+    {name: 'Petya',age: 38},
+  ] as User[];
 }
