0524ca26351445dec07f8649af29396e242ccf9b
lyklecharova <lklecharova9@gmail.com>
2024-02-25 12:11:38 +0200

    update

--- DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -16,12 +16,27 @@ export class AppComponent {
     { name: 'Petya', age: 28 },
   ];
 
+  constructor() {
+    setInterval(() => {
+      this.users.push({
+        name: 'DemoName',
+        age: 0,
+      });
+      console.log('User has been added!');
+    }, 3000);
+  }
+
   addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
     const user = {
       name: inputName.value,
       age: Number(inputAge.value),
     };
-    this.users.push(user);
+    // презаписва стойността на променливата this.users
+    // създава нов масив, който съдържа всички елементи от this.users,
+    // след което добавя новия потребител user към края на този нов масив.
+    this.users = [...this.users, user];
+
+    // this.users.push(user);
 
     inputName.value = '';
     inputAge.value = '';
--- DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
+++ DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { User } from '../types/User';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersListComponent {
   @Input() users: User[] = [];
+
+  // cd -> change detection
+  // ChangeDetectorRef ->  ръчно актуализира визуалният интерфейс на компонента, когато има промени
+  constructor(private cd: ChangeDetectorRef){}
+
+  // ngOnChanges "хваща" (или реагира на) всички промени на входящите параметри, които са свързани с компонентата.
+  ngOnChanges() {
+    console.log('invoked from ngOnChanges');
+  }
+
+  // refresh(){
+  //   this.cd.detectChanges();
+  // }
 }
