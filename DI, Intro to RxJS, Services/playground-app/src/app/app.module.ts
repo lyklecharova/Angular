f282357f201953e185d52aced63cecd91e9c1389
lyklecharova <lklecharova9@gmail.com>
2024-02-28 10:31:22 +0200

    Update app.module.ts

--- DI, Intro to RxJS, Services/playground-app/src/app/app.module.ts
+++ DI, Intro to RxJS, Services/playground-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserService } from './user.service';
+import { Observable } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent, UserListComponent, UserItemComponent],
@@ -13,3 +14,39 @@ import { UserService } from './user.service';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// Observables
+// sync analogy
+[1, 2, 3, 4].map((x) => x * 2);
+
+// async example
+
+// const o = new Observable((observer) => {
+//   observer.next(200);
+//   observer.next(210);
+// });
+
+// o.subscribe((data) => {
+//   console.log('from observervable form:', data);
+// });
+
+const interval = (intervalValue: number) => {
+  const o = new Observable<number>((observer) => {
+    let counter = 0;
+
+    const timer = setInterval(() => {
+      observer.next(counter++);
+    }, intervalValue);
+
+    // clear data on destroy
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  return o;
+};
+
+interval(2000).subscribe((data) => {
+  console.log('data from interval', data);
+});
