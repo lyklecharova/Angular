c5f55bcdd30fe20b5799f2f3e8b9d0c6cbb8a912
lyklecharova <lklecharova9@gmail.com>
2024-03-02 14:38:16 +0200

    add

--- Components/Components-Worshsop/components-workshop/src/app/api.service.ts
+++ Components/Components-Worshsop/components-workshop/src/app/api.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment.development';
 import { Theme } from './types/theme';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,12 @@ export class ApiService {
     return this.http.get<Theme[]>(`${apiUrl}/themes`);
   }
 
-  getPosts(limit?: number) {}
+  getPosts(limit?: number) {
+    const { apiUrl } = environment;
+    let url = `${apiUrl}/posts`;
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+    return this.http.get<Post[]>(url);
+  }
 }
