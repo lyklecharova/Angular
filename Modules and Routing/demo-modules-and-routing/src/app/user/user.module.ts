6a76f44cfde939d398fd39259d770459465f4d18
lyklecharova <lklecharova9@gmail.com>
2024-03-04 12:41:09 +0200

    Create user.module.ts

---
+++ Modules and Routing/demo-modules-and-routing/src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
