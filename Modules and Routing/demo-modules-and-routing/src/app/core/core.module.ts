64b5dd905ada101a98f9d42d39373996799db4c7
lyklecharova <lklecharova9@gmail.com>
2024-03-05 12:29:47 +0200

    update

--- Modules and Routing/demo-modules-and-routing/src/app/core/core.module.ts
+++ Modules and Routing/demo-modules-and-routing/src/app/core/core.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
   declarations: [
-    GlobalLoaderComponent
+    GlobalLoaderComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
 
   ],
-  exports: [GlobalLoaderComponent]
+  exports: [GlobalLoaderComponent, NavigationComponent]
 })
 export class CoreModule { }
--- Modules and Routing/demo-modules-and-routing/src/app/todo/todo-list/todo-list.component.ts
+++ Modules and Routing/demo-modules-and-routing/src/app/todo/todo-list/todo-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-
+  
 }
