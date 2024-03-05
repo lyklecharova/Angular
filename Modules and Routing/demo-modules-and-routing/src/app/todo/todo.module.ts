6368513fafd4a720c73b8d692c6cfabe8f1ce715
lyklecharova <lklecharova9@gmail.com>
2024-03-05 12:17:09 +0200

    Create todo.module.ts

---
+++ Modules and Routing/demo-modules-and-routing/src/app/todo/todo.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [TodoListComponent]
+})
+export class TodoModule { }
