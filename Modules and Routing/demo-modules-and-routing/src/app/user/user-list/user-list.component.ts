29807c20a46a28267a4becc8547355e5a9563f19
lyklecharova <lklecharova9@gmail.com>
2024-03-05 11:01:10 +0200

    Update user-list.component.ts

--- Modules and Routing/demo-modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Modules and Routing/demo-modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  userList: User[] = [];
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.fetchUsers().subscribe((users) => {
+      this.userList = users;
+    });
+  }
 }
