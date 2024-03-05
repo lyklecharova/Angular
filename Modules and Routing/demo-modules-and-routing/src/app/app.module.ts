1c645323ad42f1bcf6c1728366efcc0fa449d22c
lyklecharova <lklecharova9@gmail.com>
2024-03-05 11:01:03 +0200

    Update app.module.ts

--- Modules and Routing/demo-modules-and-routing/src/app/app.module.ts
+++ Modules and Routing/demo-modules-and-routing/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    UserModule
+    UserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
