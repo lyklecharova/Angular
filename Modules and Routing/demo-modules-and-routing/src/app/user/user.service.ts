8b6f22aa7835357fcb871df6504c1ea6c58eaba2
lyklecharova <lklecharova9@gmail.com>
2024-03-05 11:01:14 +0200

    Create user.service.ts

---
+++ Modules and Routing/demo-modules-and-routing/src/app/user/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService implements OnInit{
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    
+  }
+
+  fetchUsers(){
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  }
+}
