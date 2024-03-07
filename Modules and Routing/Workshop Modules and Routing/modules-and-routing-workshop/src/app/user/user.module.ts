9f9dda62170bffb39b9b5d800c505802dc9228d9
lyklecharova <lklecharova9@gmail.com>
2024-03-07 20:47:59 +0200

    Update user.module.ts

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/user.module.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/user.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
-import { UserRoutingModule } from './user-routing.modue';
-
-
+import { UserRoutingModule } from './user-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +14,8 @@ import { UserRoutingModule } from './user-routing.modue';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    RouterModule
   ]
 })
 export class UserModule { }
