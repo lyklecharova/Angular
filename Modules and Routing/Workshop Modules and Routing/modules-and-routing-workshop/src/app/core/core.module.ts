20c52bf3322456a4dabb23a5aab59c67822b7866
lyklecharova <lklecharova9@gmail.com>
2024-03-07 20:47:39 +0200

    Update core.module.ts

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/core/core.module.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
-
-
 
 @NgModule({
   declarations: [
@@ -13,7 +12,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+
   ],
   exports:[HeaderComponent, FooterComponent]
 })
