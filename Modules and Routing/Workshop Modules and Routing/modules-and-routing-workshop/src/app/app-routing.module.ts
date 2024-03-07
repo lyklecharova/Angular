12be877942d9966c36c9973519effbed72e6c9f4
lyklecharova <lklecharova9@gmail.com>
2024-03-07 19:52:47 +0200

    Update app-routing.module.ts

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/app-routing.module.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 
-
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
