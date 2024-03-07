91fe64008ed9bfd40154fa5a5a37cf4c171ada5f
lyklecharova <lklecharova9@gmail.com>
2024-03-07 20:47:36 +0200

    Update app.module.ts

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/app.module.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/app.module.ts
@@ -1,37 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { PostsListComponent } from './posts-list/posts-list.component';
-import { ThemeComponent } from './theme/theme.component';
-import { ThemesListComponent } from './themes-list/themes-list.component';
-import { CoreModule } from './core/core.module';
-import { HomeComponent } from './home/home.component';
-import { NewThemeComponent } from './theme/new-theme/new-theme.component';
-import { UserModule } from './user/user.module';
 
+import { CoreModule } from './core/core.module';
+
+import { HomeComponent } from './home/home.component';
+import { AddThemeComponent } from './theme/add-theme/add-theme.component';
+import { UserModule } from './user/user.module';
+import { ThemeModule } from './theme/theme.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    PostsListComponent,
-    ThemeComponent,
-    ThemesListComponent,
     HomeComponent,
-    NewThemeComponent
-    
+    AddThemeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
+    HttpClientModule,
+    UserModule,
+    ThemeModule,
     AppRoutingModule,
-    UserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
