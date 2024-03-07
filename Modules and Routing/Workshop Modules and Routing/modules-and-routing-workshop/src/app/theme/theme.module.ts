6d972d35783ca2aaedd5cf31662c0ef0c5a93e91
lyklecharova <lklecharova9@gmail.com>
2024-03-07 18:51:37 +0200

    update

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/theme/theme.module.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/theme/theme.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NwThemeComponent } from './nw-theme/nw-theme.component';
-import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 
 
 @NgModule({
   declarations: [
-    NwThemeComponent,
-    NewThemeComponent,
     CurrentThemeComponent
   ],
   imports: [
