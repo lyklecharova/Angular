649dbc872f243f70f50f5c32b45f295456f77f9f
lyklecharova <lklecharova9@gmail.com>
2024-03-08 16:37:13 +0200

    Update home.component.ts

--- Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/home/home.component.ts
+++ Modules and Routing/Workshop Modules and Routing/modules-and-routing-workshop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private userService: UserService){}
 
+  get isLoggedIn():boolean{
+    return this.userService.isLogged;
+  }
 }
